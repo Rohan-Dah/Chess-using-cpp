9f117318ef940d4d9f8de42e8c40eed49f5465ca
Rohan Dahibhate <109022906+Rohan-Dah@users.noreply.github.com>
2023-04-03 19:59:01 +0530

    Update ChessGame.cpp

--- ChessGame.cpp
+++ ChessGame.cpp
@@ -1,32 +1,3 @@
-/*File Information
-File Name: Chess.cpp
-Language: OOP using C++
-
-Developed By: Wajahat Karim
-              Sheeraz Ahmed
-
-Supervisior: Sir Shahid Razzaq
-*/
-
-
-
-/*Game Information
-Chess Game using OOP in C++
-Y-Axis Label Coordinates (14,1)
-X-Axis Label Coordinates (18,24)
-Dimension of Each Square/Step = (5 characters) x (3 Line Break)
-X-Distance to First Component = 17 Characters
-Y-Distance to First Component = 1 Line Break
-Coordinates of First Component = (18,1)
-Horizontal Distance from One Component to Other = 5 Characters
-Vertical Distance from One Component to Other = 3 Line Breaks
-Pawn (Soldier) = 1
-Knight (Horse) = 2
-Bishop (Camel) = 3
-Rook (Elephant/Castle) = 4
-Queen = 5
-King = 6
-*/
 #include<iostream>
 #include<windows.h>
 #include<conio.h>
